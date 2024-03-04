cf78e9da3da1c9af17f52f284d63d3bdb7423a1c
sujansuja <sujansuja543@gmail.com>
2024-03-04 09:12:57 +0530

    Template ready

--- main.py
+++ main.py
@@ -1,47 +1,48 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, render_template, request, jsonify
 import os
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 CORS(app)
+app.debug = True
 
-@app.route('/')
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS 
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return send_file('index.html')
-
-
-@app.route('/', methods=['POST'])
-def handle_post_request():
     if request.method == 'POST':
-        print("Recieved POST request")
+        print("Got POST method")
         try:
-            class_count = request.form.get('classCount', 0)
-            print(f"Class Count: {class_count}")
-            img_data = request.files.getlist('imgData[]')
+            count = request.form.get('classCount')
 
-            upload_folder = 'upload_folder'
-            os.makedirs(upload_folder, exist_ok=True)
+            for i in range(int(count)):
+                classNum = "className" + str(i)
+                className = request.form.get(classNum)
 
-            # Process uploaded files
-            for image_file in img_data:
-                class_name = request.form.get('className')
-                class_folder = os.path.join(upload_folder, class_name)
+                class_folder = os.path.join(app.config['UPLOAD_FOLDER'], className)
                 os.makedirs(class_folder, exist_ok=True)
 
-                filename = image_file.filename
-                filepath = os.path.join(class_folder, filename)
-                image_file.save(filepath)
-                print(f"Saved the file: {filename} to {filepath}")
+                imageName = "images" + str(i)
+                files = request.files.getlist(imageName)
+                for i,img in enumerate(files):
+                    if allowed_file(img.filename):
+                        filename = str(i) + ".png"
+                        img.save(os.path.join(class_folder, filename))
+                        print(i, end="")
 
-            return jsonify({'message': 'Files successfully uploaded and processed!'}), 200
-
+            return jsonify({"message": "Images uploaded successfully!"}),200
+        
         except Exception as e:
             print(f"Error: {e}")
             return jsonify({'error': 'Internal server error'}), 500
-
-    else:
-        return "Only POST requests are allowed", 405
+        
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
