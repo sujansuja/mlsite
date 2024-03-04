b1d73adc34ae6089357cb39dbd5acac11284bae1
sujansuja <sujansuja543@gmail.com>
2024-03-04 09:48:20 +0530

    Deleting uploaded folder after 5 sec

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import os
 from flask_cors import CORS
+import shutil
+import time
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +12,18 @@ UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # not including  symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+    
+    return total_size / (1024 * 1024)
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS 
@@ -35,6 +49,11 @@ def index():
                         filename = str(i) + ".png"
                         img.save(os.path.join(class_folder, filename))
                         print(i, end="")
+            print("Uploaded file size: ", end="")
+            print(get_size("./uploads"), 'MB')
+
+            time.sleep(5)
+            shutil.rmtree("./uploads", ignore_errors=True)
 
             return jsonify({"message": "Images uploaded successfully!"}),200
         
@@ -43,6 +62,10 @@ def index():
             return jsonify({'error': 'Internal server error'}), 500
         
     return render_template('index.html')
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
